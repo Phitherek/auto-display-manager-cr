520f40ad8a9677784cfd68e93ec1482d65d25ae7
Phitherek_ <phitherek@gmail.com>
2017-08-15 21:06:49 +0200

    Working configurator.

---
+++ autodm-config.cr
@@ -0,0 +1,100 @@
+require "./config"
+
+puts "AutoDisplayManager Configurator v. 0.1-cr (C) 2017 by Phitherek_"
+puts
+homepath = ENV["HOME"]
+config = AutoDisplayManagerCR::Config.new("#{homepath}/.auto-display-manager")
+if ARGV.size >= 1
+    unless %w(usage help list set unset switch next_profile remove).includes?(ARGV[0].downcase)
+        profile = ARGV.delete_at(0).downcase
+    else
+        profile = nil
+    end
+    if ARGV[0].downcase == "usage"
+        puts "Usage: #{$0} [profile_name] <command> [parameters]"
+        puts "See 'help' command for list of commands and parameters"
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "help"
+        puts "Usage: #{$0} [profile_name] <command> [parameters]"
+        puts
+        puts "Available commands: "
+        puts
+        puts "  * help - display this help message"
+        puts "  * usage - display the usage line"
+        puts "  * list - list the current configuration"
+        puts "  * set - set a display definition. Usage: set <name> <string> [xrandr_parameters]"
+        puts "  * unset - unset a display definition. Usage: unset <name>"
+        puts "  * switch - switch to a profile defined in profile_name"
+        puts "  * next_profile - switch to next profile in profile list"
+        puts "  * remove - remove a profile defined in profile name"
+        puts
+        puts "Profile name is used only by list, set and unset commands. When profile name is not given, 'default' is assumed."
+        puts
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "list"
+        puts "Current configuration: "
+        config.display(profile)
+    elsif ARGV[0].downcase == "set"
+        if ARGV.size < 3
+            puts "Usage: #{$0} [profile_name] set <name> <string> [xrandr_parameters]"
+            puts "See 'help' command for full list of commands and parameters"
+        else
+            name = ARGV[1].downcase
+            string = ARGV[2]
+            params = ARGV[3..-1].join(" ")
+            config.set_display(name, string, params, profile)
+            config.save
+            config.reset
+            puts "Done!"
+            puts "Current configuration: "
+            config.display(nil)
+        end
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "unset"
+        if ARGV.size < 2
+            puts "Usage: #{$0} [profile_name] unset <name>"
+            puts "See 'help' command for full list of commands and parameters"
+        else
+            name = ARGV[1].downcase
+            config.remove_display(name,profile)
+            config.save
+            config.reset
+            puts "Done!"
+            puts "Current configuration: "
+            config.display(nil)
+        end
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "switch"
+        config.switch_profile(profile)
+        config.save
+        config.reset
+        `notify-send -t 2000 -a autodm-config -c autodm-config 'AutoDisplayManager profile switched!' 'Switched to profile: #{config.selected_profile}'`
+        puts "Done!"
+        puts "Current configuration: "
+        config.display(nil)
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "next_profile"
+        config.next_profile!
+        config.save
+        config.reset
+        `notify-send -t 2000 -a autodm-config -c autodm-config 'AutoDisplayManager profile switched!' 'Switched to profile: #{config.selected_profile}'`
+        puts "Done!"
+        puts "Current configuration: "
+        config.display(nil)
+        abort(nil, 0)
+    elsif ARGV[0].downcase == "remove"
+        if profile.nil?
+            puts "Profile to delete not given! See 'help' for usage details."
+        else
+            config.remove_profile(profile)
+            config.save
+            config.reset
+            puts "Done!"
+            puts "Current configuration: "
+            config.display(nil)
+        end
+        abort(nil, 0)
+    end
+end
+puts "Usage: #{$0} [profile_name] <command> [parameters]"
+puts "See 'help' command for list of commands and parameters"
--- config.cr
+++ config.cr
@@ -6,61 +6,104 @@ module AutoDisplayManagerCR
         def initialize(path : String)
             FileUtils.mkdir_p(path)
             @filepath = "#{path}/config.yml"
+            config = {} of String => (String|Array(String)|Hash(String, String|Hash(String, String))) 
             if File.exists?(@filepath)
-                @config = YAML.parse(File.read(@filepath))
+                config = from_yaml(YAML.parse(File.read(@filepath)))
             end
-            @config ||= {} of String => (String|Array(String)|Hash(String, String|Hash(String, String)))
-            @config["displays"] ||= [] of String
-            @config["profiles"] ||= [] of String
+            config["displays"] ||= [] of String
+            config["profiles"] ||= [] of String
+            @config = config.as(Hash(String, (String|Array(String)|Hash(String, String|Hash(String, String))))) 
         end
 
         def set_display(name : String, string : String, options : String, profile = "default")
-            @config[profile] ||= {} of String => (String|Hash(String, String))
-            @config[profile][name] ||= {} of String => String
-            @config[profile][name]["str"] = string
-            @config[profile][name]["options"] = options
-            @config["displays"] << name
-            @config["displays"].uniq!
-            @config["profiles"] << profile
-            @config["profiles"].uniq!
+            profile = profile.to_s
+            config = @config 
+            config[profile] ||= {} of String => (String|Hash(String, String))
+            profile_data = config[profile]
+            if profile_data.is_a?(Hash)
+                profile_data[name] ||= {} of String => String
+                display_data = profile_data[name]
+                if display_data.is_a?(Hash)
+                    display_data["str"] = string
+                    display_data["options"] = options
+                    profile_data[name] = display_data
+                end
+            end
+            config[profile] = profile_data
+            displays = config["displays"]
+            if displays.is_a?(Array)
+                displays << name
+                displays.uniq!
+            end
+            config["displays"] = displays
+            profiles = config["profiles"]
+            if profiles.is_a?(Array)
+                profiles << profile
+                profiles.uniq!
+            end
+            config["profiles"] = profiles
+            @config = config
         end
 
         def switch_profile(profile = "default")
+            profile = profile.to_s
             @config["selected_profile"] = profile if @config["profiles"].includes?(profile)
         end
 
         def next_profile!
-            sorted_profiles = @config["profiles"].sort
-            current_idx = sorted_profiles.find_index(@config["selected_profile"])
-            current_idx ||= -1
-            current_idx = current_idx + 1
-            current_idx = 0 if current_idx >= sorted_profiles.count
-            @config["selected_profile"] = sorted_profiles[current_idx]
+            sorted_profiles = @config["profiles"]
+            if sorted_profiles.is_a?(Array)
+                sorted_profiles.sort
+                current_idx = sorted_profiles.index(@config["selected_profile"])
+                current_idx ||= -1
+                current_idx = current_idx + 1
+                current_idx = 0 if current_idx >= sorted_profiles.size
+                @config["selected_profile"] = sorted_profiles[current_idx]
+            end
         end
 
         def remove_profile(profile : String)
-            if @config["profiles"].include?(profile)
-                @config.delete(profile)
-                @config["profiles"].delete(profile)
-            end
-            @config["displays"] = [] of String
-            @config["profiles"].each do |profile|
-                @config[profile].each_key do |k|
-                    @config["displays"] << k
+            profiles = @config["profiles"]
+            if profiles.is_a?(Array)
+                if profiles.includes?(profile)
+                    @config.delete(profile)
+                    profiles.delete(profile)
                 end
+                displays = [] of String
+                profiles.each do |profile|
+                    profile_data = @config[profile]
+                    if profile_data.is_a?(Hash)
+                        profile_data.each_key do |k|
+                            displays << k
+                        end
+                    end
+                    @config[profile] = profile_data
+                end
+                displays.uniq!
+                @config["displays"] = displays
             end
-            @config["displays"].uniq!
+            @config["profiles"] = profiles
         end
 
         def remove_display(name : String, profile = "default")
-            @config[profile].delete(name)
-            @config["displays"] = [] of String
-            @config["profiles"].each do |profile|
-                @config[profile].each_key do |k|
-                    @config["displays"] << k
+            profile = profile.to_s
+            profile_data = @config[profile]
+            if profile_data.is_a?(Hash)
+                @config[profile].delete(name)
+                displays =  [] of String
+                profiles = @config["profiles"]
+                if displays.is_a?(Array) && profiles.is_a?(Array)
+                    profiles.each do |profile|
+                        profile_data.each_key do |k|
+                            displays << k
+                        end
+                    end
+                    displays.uniq!
                 end
+                @config["profiles"] = profiles
+                @config["displays"] = displays
             end
-            @config["displays"].uniq!
+            @config[profile] = profile_data
         end
 
         def save
@@ -71,16 +114,28 @@ module AutoDisplayManagerCR
             keys.delete("displays")
             keys.delete("profiles")
             keys.delete("selected_profile")
-            @config["displays"].each do |d|
-                keys.delete(d)
+            displays = @config["displays"]
+            if displays.is_a?(Array)
+                displays.each do |d|
+                    keys.delete(d)
+                end
             end
-            @config["profiles"].each do |p|
-                keys.delete(p)
+            profiles = @config["profiles"]
+            if profiles.is_a?(Array)
+                profiles.each do |p|
+                    keys.delete(p)
+                end
             end
             keys.each do |k|
                 @config.delete(k)
-                @config["profiles"].each do |p|
-                    @config[p].delete(k)
+                if profiles.is_a?(Array)
+                    profiles.each do |p|
+                        profile_data = @config[p]
+                        if profile_data.is_a?(Array)
+                            profile_data.delete(k)
+                        end
+                        @config[p] = profile_data
+                    end
                 end
             end
             File.open(@filepath, "w") do |f|
@@ -89,18 +144,29 @@ module AutoDisplayManagerCR
         end
 
         def reset
+            config = {} of String => (String|Array(String)|Hash(String, String|Hash(String, String))) 
             if File.exists?(@filepath)
-                @config = YAML.parse(File.read(@filepath))
+                config = from_yaml(YAML.parse(File.read(@filepath)))
             end
-            @config ||= {} of String => (String|Array(String)|Hash(String, String|Hash(String, String)))
-            @config["profiles"] ||= [] of String
-            @config["displays"] ||= [] of String
-            @config["profiles"].each do |profile|
-                @config[profile].each_key do |k|
-                    @config["displays"] << k
+            config["displays"] ||= [] of String
+            config["profiles"] ||= [] of String
+            displays = config["displays"]
+            profiles = config["profiles"]
+            if displays.is_a?(Array)
+                if profiles.is_a?(Array)
+                    profiles.each do |profile|
+                        profile_data = config[profile]
+                        if profile_data.is_a?(Hash)
+                            profile_data.each_key do |k|
+                                displays << k
+                            end
+                        end
+                    end
                 end
+                displays.uniq!
             end
-            @config["displays"].uniq!
+            config["displays"] = displays
+            @config = config
         end
 
         def display(profile = nil.as(String|Nil))
@@ -112,23 +178,62 @@ module AutoDisplayManagerCR
         end
 
         def profiles
-            @config["profiles"]
+            @config["profiles"].as(Array(String))
         end
 
         def displays
-            @config["displays"]
+            @config["displays"].as(Array(String))
         end
 
         def selected_profile
-            @config["selected_profile"]
+            @config["selected_profile"].as(String)
         end
 
         def get_profile(profile = "default")
-            @config[profile]
+            @config[profile].as(Hash(String, String|Hash(String, String)))
         end
 
         def get_display(display : String, profile = "default")
-            @config[profile][display]
+            @config[profile][display].as(Hash(String, String))
+        end
+
+        private def from_yaml(yaml_root : YAML::Any)
+            ret = {} of String => (String|Array(String)|Hash(String, String|Hash(String, String))) 
+            if yaml_root.raw.is_a?(Hash)
+                yaml_root.each do |k, v|
+                    if k.raw.is_a?(String)
+                        if v.raw.is_a?(String)
+                            ret[desym_yaml_string(k.as_s)] = desym_yaml_string(v.as_s)
+                        elsif v.raw.is_a?(Array)
+                            tmp = [] of String
+                            v.each do |vv|
+                                tmp << desym_yaml_string(vv.as_s)
+                            end
+                            ret[desym_yaml_string(k.as_s)] = tmp
+                        elsif v.raw.is_a?(Hash)
+                            profile_data = {} of String => String|Hash(String, String)
+                            v.each do |vk, vv|
+                                if vv.raw.is_a?(Hash)
+                                    display_data = {} of String => String
+                                    vv.each do |vvk, vvv|
+                                        display_data[desym_yaml_string(vvk.as_s)] = desym_yaml_string(vvv.as_s)
+                                    end
+                                    profile_data[desym_yaml_string(vk.as_s)] = display_data
+                                end
+                            end
+                            ret[desym_yaml_string(k.as_s)] = profile_data
+                        end
+                    end
+                end
+            end
+            return ret                 
+        end
+
+        private def desym_yaml_string(str : String)
+            if str[0] == ':'
+                str = str[1..-1]
+            end
+            str
         end
     end
 end
